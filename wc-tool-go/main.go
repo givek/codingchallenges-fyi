b1cb1dfc9d295344059aa11840cd8507e1fd40aa
Vivek Gandharkar <vivekgandharkar@outlook.com>
2026-06-16 20:46:44 +0530

    count number of chars

--- wc-tool-go/main.go
+++ wc-tool-go/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
+	"slices"
+	"strings"
+	"unicode"
 )
 
 func SizeInBytes(r io.Reader) (int64, error) {
@@ -25,6 +29,120 @@ func SizeInBytes(r io.Reader) (int64, error) {
 	return sizeInBytes, nil
 }
 
+func NumberOfLines(r io.Reader) (int64, error) {
+	buf := make([]byte, 16*1024)
+	var totalLineCount int64 = 0
+
+	for {
+		n, err := r.Read(buf)
+
+		// 1. Always process the data read *before* checking the error
+		if n > 0 {
+			lineCount := bytes.Count(buf[:n], []byte{'\n'})
+			totalLineCount += int64(lineCount)
+		}
+
+		// 2. Now check if we hit the end of the file or an error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return totalLineCount, nil
+}
+
+func NumberOfWords(r io.Reader) (int64, error) {
+	buf := make([]byte, 16*1024)
+	var totalWordCount int64 = 0
+
+	lastWord := ""
+
+	for {
+		n, err := r.Read(buf)
+
+		if n > 0 {
+			currStr := lastWord + string(buf[:n])
+
+			isLastCharSpace := unicode.IsSpace(rune(currStr[len(currStr)-1]))
+
+			words := strings.Fields(currStr)
+
+			if len(words) > 0 {
+				if isLastCharSpace {
+					totalWordCount += int64(len(words))
+					lastWord = ""
+				} else {
+					totalWordCount += int64(len(words) - 1)
+					lastWord = words[len(words)-1]
+				}
+			}
+
+		}
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if len(lastWord) != 0 {
+		totalWordCount += 1
+	}
+
+	return totalWordCount, nil
+}
+
+func NumberOfChars(r io.Reader) (int64, error) {
+	buf := make([]byte, 16*1024)
+	var totalCharCount int64 = 0
+
+	remaining := []byte{}
+
+	for {
+		n, err := r.Read(buf)
+
+		if n > 0 {
+			currStr := slices.Concat(remaining, buf[:n])
+
+			lastNewLineIdx := bytes.LastIndex(currStr, []byte{'\n'})
+
+			if lastNewLineIdx == -1 {
+				remaining = currStr
+				continue
+			}
+
+			ws := currStr[:lastNewLineIdx+1]
+
+			remaining = currStr[lastNewLineIdx+1:]
+
+			for range string(ws) {
+				totalCharCount += 1
+			}
+
+		}
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	if len(remaining) > 0 {
+		for range string(remaining) {
+			totalCharCount += 1
+		}
+	}
+
+	return totalCharCount, nil
+}
+
 func main() {
 	f, err := os.Open("./test.txt")
 	if err != nil {
@@ -32,15 +150,29 @@ func main() {
 	}
 	defer f.Close()
 
-	sizeInBytes, err := SizeInBytes(f)
+	// _, err = SizeInBytes(f)
+	// if err != nil {
+	// 	panic(err) // TODO: Handle the error gracefully
+	// }
+
+	// totalLineCount, err := NumberOfLines(f)
+	// if err != nil {
+	// 	panic(err) // TODO: Handle the error gracefully
+	// }
+
+	// totalWordCount, err := NumberOfWords(f)
+	// if err != nil {
+	// 	panic(err) // TODO: Handle the error gracefully
+	// }
+
+	totalCharCount, err := NumberOfChars(f)
 	if err != nil {
 		panic(err) // TODO: Handle the error gracefully
 	}
-
 	// fStat, err := f.Stat()
 	// if err != nil {
 	// 	panic(err)
 	// }
 
-	fmt.Printf("%v %v\n", sizeInBytes, f.Name())
+	fmt.Printf("%v %v\n", totalCharCount, f.Name())
 }
