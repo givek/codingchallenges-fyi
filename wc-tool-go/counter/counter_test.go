a3a2e9984e3fbc9f888609c205f7eaec1cb6977e
Vivek Gandharkar <vivekgandharkar@outlook.com>
2026-06-17 20:19:24 +0530

    add more tests

--- wc-tool-go/counter/counter_test.go
+++ wc-tool-go/counter/counter_test.go
@@ -53,3 +53,135 @@ func TestSizeInBytes(t *testing.T) {
 		})
 	}
 }
+
+func TestNumberOfLines(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []byte
+		expected int64
+	}{
+		{
+			name:     "empty string",
+			input:    []byte(""),
+			expected: 0,
+		},
+		{
+			name:     "single newline character, no trailing newline",
+			input:    []byte(fmt.Sprintf("Hello, \n Hope you are doing well!")),
+			expected: 1,
+		},
+		{
+			name:     "no newline character",
+			input:    []byte("Hello 👋 世界"),
+			expected: 0,
+		},
+		{
+			name:     "large input (10 KB)",
+			input:    bytes.Repeat([]byte(fmt.Sprintf("A\n")), 10240),
+			expected: 10240,
+		},
+		{
+			name:     "control characters and mixed newlines",
+			input:    []byte("\x00\n\r\t"),
+			expected: 1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sizeInBytes, err := counter.NumberOfLines(bytes.NewBuffer(test.input))
+
+			require.NoError(t, err, "failed to parse input for test case:")
+
+			assert.Equal(t, test.expected, sizeInBytes, "expect: %v, got: %v", test.expected, sizeInBytes)
+		})
+	}
+}
+
+func TestNumberOfWords(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []byte
+		expected int64
+	}{
+		{
+			name:     "empty string",
+			input:    []byte(""),
+			expected: 0,
+		},
+		{
+			name:     "only ASCII characters",
+			input:    []byte(fmt.Sprintf("Hello, \n Hope you are doing well!")),
+			expected: 6,
+		},
+		{
+			name:     "ASCII + UTF8 characters",
+			input:    []byte("Hello 👋 世界"),
+			expected: 3,
+		},
+		{
+			name:     "large input (10 KB)",
+			input:    bytes.Repeat([]byte(fmt.Sprintf("A CB B\n")), 10240),
+			expected: 10240 * 3,
+		},
+		{
+			name:     "control characters and mixed newlines",
+			input:    []byte("\x00\n\r\t"),
+			expected: 1,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sizeInBytes, err := counter.NumberOfWords(bytes.NewBuffer(test.input))
+
+			require.NoError(t, err, "failed to parse input for test case:")
+
+			assert.Equal(t, test.expected, sizeInBytes, "expect: %v, got: %v", test.expected, sizeInBytes)
+		})
+	}
+}
+
+func TestNumberOfChars(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []byte
+		expected int64
+	}{
+		{
+			name:     "empty string",
+			input:    []byte(""),
+			expected: 0,
+		},
+		{
+			name:     "only ASCII characters",
+			input:    []byte(fmt.Sprintf("Hello, \n Hope you are doing well!")),
+			expected: 33,
+		},
+		{
+			name:     "multi-byte UTF-8 characters (emojis and kanji)",
+			input:    []byte("Hello 👋 世界"),
+			expected: 10,
+		},
+		{
+			name:     "large input (10 KB)",
+			input:    bytes.Repeat([]byte("A"), 10240),
+			expected: 10240,
+		},
+		{
+			name:     "control characters and mixed newlines",
+			input:    []byte("\x00\n\r\t"),
+			expected: 4,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			sizeInBytes, err := counter.NumberOfChars(bytes.NewBuffer(test.input))
+
+			require.NoError(t, err, "failed to parse input for test case:")
+
+			assert.Equal(t, test.expected, sizeInBytes, "expect: %v, got: %v", test.expected, sizeInBytes)
+		})
+	}
+}
